242424aed85ffc5976c28fd4f62b9ad7f399c798
IJBorda <37444801+IJBorda@users.noreply.github.com>
2022-03-09 00:07:55 +0800

    Add animations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+// Add listener
+window.addEventListener("scroll", reveal);
+document.querySelector("#navButon").addEventListener("click", showNav)
 
+// Function for reveal
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    console.log(reveals)
+    console.log("success")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowHeight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+// Function for shownav
+function showNav() {
+    navOption = document.querySelector("#navOption")
+    navOption.classList.toggle("hidden")
+}
